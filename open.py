e038482698464e6bdf20886e2f031fb8600bf2f6
Aditya Kumar Sahu <adityasahu1124@gmail.com>
2022-07-28 22:54:49 +0530

    created open.py file to view data of .npy files

---
+++ open.py
@@ -0,0 +1,13 @@
+"""
+Program to open .npy files
+"""
+import numpy as np
+print("\nTrace_Array:-")
+data=np.load("trace_array.npy")
+print(data)
+print("\nTextin_Array:-")
+data=np.load("textin_array.npy")
+print(data)
+print("\nTextout_Array:-")
+data=np.load("textout_array.npy")
+print(data)
